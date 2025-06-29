abd0ac24827ed5fa6afcb18da66cab99bf357a56
tabby-b <tabbyb@amazon.com>
2025-06-28 17:41:15 -0700

    Done

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-DATABASE_URL = "postgresql://postgres:SmCfMJbkpuSWMlHwDpWMqBTDiTOhZYtO@postgresapi-production.up.railway.app:443/railway?sslmode=require"
+DATABASE_URL = "postgresql://postgres:SmCfMJbkpuSWMlHwDpWMqBTDiTOhZYtO@postgresapi-production.up.railway.app:443/railway"
 
 
 @app.route('/')
